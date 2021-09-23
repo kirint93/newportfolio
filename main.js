86862902ab386d202a0e3d8503d83685424c0d10
kirin Thapar <kirinthapar@yandex.com>
2021-09-23 15:48:39 +0100

    new version of portfolio

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ const theYear = new Date();
 document.getElementById("year").innerHTML =`${theYear.getFullYear()}`;
 
 
+// scrollButton
+const scrollButton = document.querySelector("#scrollButton");
+
+scrollButton.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
+
+document.getElementById("clickable")
+
+
+function changeProjects() {
+    
+}
