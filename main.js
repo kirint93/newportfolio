a22e3373a24f5fa23249aa28c424df7a5a280a66
kirin Thapar <kirinthapar@yandex.com>
2021-10-22 14:08:54 +0100

    added reCAPTCHA  to form

--- main.js
+++ main.js
@@ -3,7 +3,8 @@
 const theYear = new Date();
 document.getElementById("year").innerHTML =`${theYear.getFullYear()}`;
 
-
+ const elm = document.getElementById("card-description1").offsetWidth;
+console.log(elm);
 
 // scrollButton
 const scrollButton = document.querySelector("#scrollButton");
