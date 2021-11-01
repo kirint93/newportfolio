ba603555e4520eb548f99a3004947ee7bf538ac1
kirin Thapar <kirinthapar@yandex.com>
2021-11-01 15:46:04 +0000

    updated info about django project ( coming soon)

--- main.js
+++ main.js
@@ -3,8 +3,6 @@
 const theYear = new Date();
 document.getElementById("year").innerHTML =`${theYear.getFullYear()}`;
 
- const elm = document.getElementById("card-description1").offsetWidth;
-console.log(elm);
 
 // scrollButton
 const scrollButton = document.querySelector("#scrollButton");
