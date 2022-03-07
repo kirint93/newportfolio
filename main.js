59b0610a73e8f939b87fba23ba6ad590ba62d6ab
kirin Thapar <62936453+kirint93@users.noreply.github.com>
2022-03-07 14:06:04 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -14,13 +14,3 @@ scrollButton.addEventListener("click", () => {
         behavior: "smooth",
     });
 });
-
-document.getElementById("clickableTitle")
-<<<<<<< HEAD
-
-
-function changeProjects() {
-
-}
-=======
->>>>>>> b39a5af8873a80c43fc93a7230e78750afc20e3e
