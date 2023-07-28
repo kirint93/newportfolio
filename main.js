2484a696c54019f61d69f643f68d1ab9d39c09ac
kirin <kirinthapar@yandex.com>
2023-07-28 12:56:58 +0100

    portfolio

--- main.js
+++ main.js
@@ -16,3 +16,24 @@ scrollButton.addEventListener("click", () => {
 });
 
 document.getElementById("clickableTitle")
+
+
+var theVideo = document.getElementById("myVideo");
+  document.onkeydown = function(event) {
+      switch (event.keyCode) {
+         case 37:
+              event.preventDefault();
+              
+              vid_currentTime = theVideo.currentTime;
+              theVideo.currentTime = vid_currentTime - 5;
+            break;
+         
+         case 39:
+              event.preventDefault();
+              
+              vid_currentTime = theVideo.currentTime;
+              theVideo.currentTime = vid_currentTime + 15;
+            break;
+         
+      }
+  };
